c65e8a24960ac083b3b4705849ae38bb2dec2582
Leonardo Paffi <leonardopaffi@hotmail.it>
2020-07-03 11:28:08 +0200

    more messages on screen

--- bash/gitpush.sh
+++ bash/gitpush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A simple shell script to group git commands (add, commit, push)
-
+echo 'Starting script...'
 git add .
 
 echo 'Enter the commit message:'
@@ -8,8 +8,9 @@ read commitMessage
 
 git commit -m "$commitMessage"
 
-branch=$(git branch | cut -c3- | dmenu -p "Select the branch to push: ")
+branch=$(git branch | cut -c3- | slmenu -p "Select the branch to push: ")
 
 git push origin $branch
 
+echo 'Press any key to exit...'
 read
